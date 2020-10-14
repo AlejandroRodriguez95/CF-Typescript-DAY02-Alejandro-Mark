e4e427a1a1369eced6ba310c3cba722151e62a8c
Mark.Etzel@gmx.email <Mark.Etzel@gmx.de>
2020-10-14 10:38:39 +0200

    basic done

--- basic/basic1/js/script.js
+++ basic/basic1/js/script.js
@@ -1,16 +1,45 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Human = /** @class */ (function () {
     function Human(name, age, job) {
+        if (job === void 0) { job = "Web Developer"; }
         this.name = "";
-        this.jobTitle = "Web Developer";
         this.name = name;
         this.age = age;
         this.jobTitle = job;
     }
     Human.prototype.hello = function () {
-        document.write("Hello there, my name is " + this.name + ", and I am " + this.age + " years old, and I am a " + this.jobTitle);
+        var result = "Hello there, my name is " + this.name + ", and I am " + this.age + " years old, and I am a " + this.jobTitle + ".";
+        return result;
     };
     return Human;
 }());
-var newHuman = new Human("Mark", 35);
+var newHuman = new Human("Mark", 30);
 console.table(newHuman);
-newHuman.hello();
+var Employee = /** @class */ (function (_super) {
+    __extends(Employee, _super);
+    function Employee(name, age, job, salary, location) {
+        if (job === void 0) { job = "Web Developer"; }
+        var _this = _super.call(this, name, age, job) || this;
+        _this.salary = salary;
+        _this.location = location;
+        return _this;
+    }
+    Employee.prototype.hello = function () {
+        return _super.prototype.hello.call(this) + " I earn " + this.salary + " and work at " + this.location + ".";
+    };
+    return Employee;
+}(Human));
+var newEmployee = new Employee("Mark", 30, "Developer", "shitloads of money", "shady company");
+document.write(newEmployee.hello());
--- basic/basic1/js/script.ts
+++ basic/basic1/js/script.ts
@@ -1,21 +1,34 @@
 class Human{
     name = "";
     age:number;
-    jobTitle = "Web Developer";
-    constructor(name:string, age:number, job?:string)
+    jobTitle:string;
+    constructor(name:string, age:number, job:string= "Web Developer")
     {
         this.name = name;
         this.age = age;
         this.jobTitle = job;
-    }
+        } 
     hello(){
-        document.write(`Hello there, my name is ${this.name}, and I am ${this.age} years old, and I am a ${this.jobTitle}`);
+        var result = `Hello there, my name is ${this.name}, and I am ${this.age} years old, and I am a ${this.jobTitle}.`
+        return result;
     }
 }
 
-let newHuman = new Human("Mark", 35);
+var newHuman = new Human("Mark", 30);
 
 console.table(newHuman);
-newHuman.hello();
 
-
+class Employee extends Human {
+    salary:any;
+    location:string;
+    constructor(name:string, age:number, job:string= "Web Developer", salary:any, location:string) {
+        super(name, age, job);
+        this.salary = salary;
+        this.location = location;
+    }
+    hello() {
+        return `${super.hello()} I earn ${this.salary} and work at ${this.location}.`;
+    }
+}
+var newEmployee = new Employee("Mark", 30, "Developer", "shitloads of money", "shady company");
+document.write(newEmployee.hello())
