8b119f59a54569cc06f6b1890e924c133a8d2809
Alejandro <alejandro.rodriguez95@gmail.com>
2020-10-14 15:19:14 +0200

    finished challenge, advanced

--- challenge/js/script.js
+++ challenge/js/script.js
@@ -1,15 +1,27 @@
-$(function () {
-    console.log("Hello World!");
-    var Profile = /** @class */ (function () {
-        function Profile(name, image, description) {
-            this.favorite = false;
-            this.name = name;
-            this.image = image;
-            this.description = description;
-        }
-        return Profile;
-    }());
-    var person0 = new Profile("Anna", "../img/person0", "placeholder");
-    person0.favorite = true;
-    console.table(person0);
+var Profile = /** @class */ (function () {
+    function Profile(name, image, description) {
+        this.favorite = false;
+        this.name = name;
+        this.image = image;
+        this.description = description;
+    }
+    return Profile;
+}());
+var person0 = new Profile("Anna", "img/person0.jpg", "placeholder");
+var person1 = new Profile("Anita", "img/person1.jpg", "placeholder");
+var person2 = new Profile("Anota", "img/person2.jpg", "placeholder");
+var person3 = new Profile("Girl4", "img/person3.jpg", "placeholder");
+var person4 = new Profile("Guy1", "img/person4.jpg", "placeholder");
+var person5 = new Profile("Guywithroses", "img/person5.jpg", "placeholder");
+var people = [person0, person1, person2, person3, person4, person5];
+person0.favorite = true;
+console.table(people);
+$(document).ready(function () {
+    for (var x in people) {
+        $("#all").append("\n        <div class='p-5 col-lg-3' id=\"" + x + "\">\n        <div class=\"card p-2\">\n        <img class=\"card-img-top\" src=\"" + people[x].image + "\" alt=\"Card image cap\">\n        <div class=\"text-center card-body\">\n          <h5 class=\"card-title\">" + people[x].name + "</h5>\n          <p class=\"card-text\">" + people[x].description + "</p>\n          <a class=\"btn bg-light border-danger text-secondary btn-primary\">&hearts;</a>\n        </div>\n        </div>\n      </div>\n        ");
+    }
+    $(".btn").one('click', function () {
+        var index = $(this).parent().parent().parent().attr('id');
+        $("#fav").append("\n        <div class='text-white p-5 col-lg-3'>\n        <div class=\"bg-danger card p-2\">\n        <img class=\"card-img-top\" src=\"" + people[index].image + "\" alt=\"Card image cap\">\n        <div class=\"text-center card-body\">\n          <h5 class=\"card-title\">" + people[index].name + "</h5>\n          <p class=\"card-text\">" + people[index].description + "</p>\n        </div>\n        </div>\n      </div>\n        ");
+    });
 });
--- challenge/js/script.ts
+++ challenge/js/script.ts
@@ -1,20 +1,56 @@
-$(() => {
-    console.log("Hello World!");
-
-    class Profile {
-        name:string;
-        image:string;
-        description:string;
-        favorite:boolean = false;
-        constructor(name:string, image:string, description:string) {
-            this.name = name;
-            this.image = image;
-            this.description = description;           
-        }
+class Profile {
+    name: string;
+    image: string;
+    description: string;
+    favorite: boolean = false;
+    constructor(name: string, image: string, description: string) {
+        this.name = name;
+        this.image = image;
+        this.description = description;
     }
-    
-    var person0 = new Profile("Anna", "../img/person0","placeholder");
-    person0.favorite = true;
-    console.table(person0);
+}
+
+var person0 = new Profile("Anna", "img/person0.jpg", "placeholder");
+var person1 = new Profile("Anita", "img/person1.jpg", "placeholder");
+var person2 = new Profile("Anota", "img/person2.jpg", "placeholder");
+var person3 = new Profile("Girl4", "img/person3.jpg", "placeholder");
+var person4 = new Profile("Guy1", "img/person4.jpg", "placeholder");
+var person5 = new Profile("Guywithroses", "img/person5.jpg", "placeholder");
+
+var people: Array<any> = [person0, person1, person2, person3, person4, person5];
+person0.favorite = true;
+console.table(people);
+
+$(document).ready(function () {
+
+    for (let x in people) {
+        $("#all").append(`
+        <div class='p-5 col-lg-3' id="${x}">
+        <div class="card p-2">
+        <img class="card-img-top" src="${people[x].image}" alt="Card image cap">
+        <div class="text-center card-body">
+          <h5 class="card-title">${people[x].name}</h5>
+          <p class="card-text">${people[x].description}</p>
+          <a class="btn bg-light border-danger text-secondary btn-primary">&hearts;</a>
+        </div>
+        </div>
+      </div>
+        `)
+    }
+
+    $(`.btn`).one('click', function () {
+        var index = $(this).parent().parent().parent().attr('id');
+        $("#fav").append(`
+        <div class='text-white p-5 col-lg-3'>
+        <div class="bg-danger card p-2">
+        <img class="card-img-top" src="${people[index].image}" alt="Card image cap">
+        <div class="text-center card-body">
+          <h5 class="card-title">${people[index].name}</h5>
+          <p class="card-text">${people[index].description}</p>
+        </div>
+        </div>
+      </div>
+        `);
+    });
 
 });
